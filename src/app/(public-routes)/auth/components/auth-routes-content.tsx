5217fe33703b4d3a3bb7eeef2d1784103ddfaeb2
Leonardo Braga <programer_frontbraga@outlook.com>
2025-05-28 22:54:29 -0300

    build: add scripts to deploy on vercel

--- src/app/(public-routes)/auth/components/auth-routes-content.tsx
+++ src/app/(public-routes)/auth/components/auth-routes-content.tsx
@@ -23,8 +23,8 @@ export function AuthRoutesContent() {
 
 	return (
 		<div className="flex w-full max-w-[450px] flex-col items-center rounded-t-2xl border-t-2 p-2 pt-4 md:w-1/2 md:rounded-none md:border-0">
-			<h3 className="mb-6 w-42 text-center font-semibold text-[#fafafa] md:w-full md:text-center md:text-3xl">
-				Seja Bem-Vindo(a) à <span className="font-lexend text-[#7f22fe]">Nome da Empresa</span>!
+			<h3 className="md:mb8 mb-6 text-center font-semibold text-[#fafafa] md:text-3xl">
+				Olá, Seja <span className="font-lexend text-[#7f22fe]">Bem-Vindo(a)</span>!
 			</h3>
 			<div className="flex w-full items-center justify-between gap-6 md:max-w-[250px] md:flex-col">
 				<CustomAuthLink href={AuthRoutes.SIGN_IN} title="Entrar" />
